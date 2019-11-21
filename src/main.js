9b96c39a5d102a98c65e7af6c6d6c8b20305bf06
zubair001 <zubairtub@gmail.com>
2019-11-21 01:36:24 +0100

    added form

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
